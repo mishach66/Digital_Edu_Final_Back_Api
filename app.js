99661bd12791cc42e3f872b3f7f2f954f7e3c45c
demna-miqava <demnamikava11@gmail.com>
2022-07-19 19:34:53 +0400

    connect to db

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`server running on PORT=${PORT}`);
-});
+(async () => {
+  try {
+    await mongoose.connect(process.env.MONGOOSE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    app.listen(PORT, () => {
+      console.log(`server running on PORT=${PORT}`);
+    });
+  } catch (err) {
+    console.log("error while starting", err);
+  }
+})();
