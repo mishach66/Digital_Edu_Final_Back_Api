833f99ace082960c077ad0386f6dff261ddadfd5
demna-miqava <demnamikava11@gmail.com>
2022-07-19 21:38:54 +0400

    add role middleware

---
+++ middlewares/role.middleware.js
@@ -0,0 +1,10 @@
+const roleMiddleware = async (req, res, next) => {
+  let role = req.user.role;
+
+  if (!role.includes("admin")) {
+    res.status(403).json({ message: "You are not authorized for this route!" });
+    return;
+  }
+
+  next();
+};
